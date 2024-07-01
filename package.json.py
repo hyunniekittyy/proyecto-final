0c26f0beaca2ca3d6ce6e4e008afbd0311cc8a9f
hyunniekittyy <marcheline.aldana@correo.unimet.edu.ve>
2024-06-30 22:17:42 -0400

    Add files via upload

---
+++ package.json.py
@@ -0,0 +1,15 @@
+{
+  "name": "api-proyecto",
+  "version": "1.0.0",
+  "description": "## Productos",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Jose Roberto Quevedo Gabizon",
+  "license": "ISC",
+  "dependencies": {
+    "@faker-js/faker": "^8.2.0",
+    "dayjs": "^1.11.10"
+  }
+}
